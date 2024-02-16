8c90666f1ccd7f5bed0c1f9d6aa733de0a7777f7
D1v3 <d1v3@MacBook-Pro-de-D1v3.local>
2024-02-16 11:37:58 -0500

    update methods

--- rename_video_clips.sh
+++ rename_video_clips.sh
@@ -7,15 +7,24 @@
 # -t timefmt  Display timestamps using the specified format.  This format is passed directly to strftime(3).
     # https://ss64.com/mac/syntax-strftime.html
 
-file_types=( *.MP4 *.MOV *.HEIC)
 
+cd $1
+echo -e "
+         \n-------------------------------------------------------------
+         \nCurrent working directory:
+         $PWD
+         \nLooking for files in this directory:
+         \n$1
+        "
+ls -la
+
+file_types=( *.MP4 *.mp4 *.MOV *.HEIC)
 createNewName () {
     creation_date=$(stat -f "%SB" -t "%m_%d_%Y_%H_%M%p" "$1")
     extension="${1##*.}"
     filename_no_ext="${1%.*}"
     echo "$creation_date-$filename_no_ext.$extension"
 }
-
 
 echo -e "\nThe following files will be renamed as follows:\n
 -------------------------------------------------------------"
